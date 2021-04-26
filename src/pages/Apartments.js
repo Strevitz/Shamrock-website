c3fb4d193080250dde756c080764b480b3e20aa7
Arthur <Strevitz@github.com>
2021-04-26 21:36:24 +0200

    zdjecie artur

--- src/pages/Apartments.js
+++ src/pages/Apartments.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import avatar from "../images/avatar.jpg";
 import stefan from "../images/stefan.jpg";
+import artur from "../images/artur.jpg";
 import lukas from "../images/lukas.jpg";
 import Title from "../components/Title";
 import Footer from "../components/Footer";
@@ -16,7 +17,7 @@ const Apartments = () => {
 
       <section className="services3">
         <div className="employee-box">
-          <img className="avatar" src={avatar} alt="A Plant Company" />
+          <img className="avatar" src={artur} alt="A Plant Company" />
           <p className="employee">Arthur - Barber</p>
         </div>
         <div className="employee-box">
